edacf38a41420cb0ea123d4bb717cd6e9775de7c
Alejandro Garcia Miravet <agarciamiravet@gmail.com>
2024-04-13 07:15:13 +0200

    change backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+   resource_group_name  = "rg-globalazure-2022-spain-terraform"
+   storage_account_name = "stgazspainterraform"
+  container_name       = "tfstate"
+   key                  = "terraform.remote.tfstate"
+ }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
